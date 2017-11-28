020828e0ae4f94fa2c2205b28109d5d22feca261
Mathias <info@ccu-historian.de>
2017-11-28 07:42:17 +0100

    groovy updated, bump version

--- ccu-historian/src/mdz/ccuhistorian/Main.groovy
+++ ccu-historian/src/mdz/ccuhistorian/Main.groovy
@@ -26,7 +26,7 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class Main {
 
-	public static String version='1.1.1beta2'
+	public static String version='1.2.0alpha1'
 	
 	private static final Logger log=Logger.getLogger(Main.class.name)
 	private static Main main
