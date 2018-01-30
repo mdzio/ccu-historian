9471b42c2a8db5013fbae1a0eda6c7386db7f6a2
Mathias <info@ccu-historian.de>
2018-01-30 20:57:36 +0100

    bump version to release 1.2.0

--- ccu-historian/src/mdz/ccuhistorian/Main.groovy
+++ ccu-historian/src/mdz/ccuhistorian/Main.groovy
@@ -26,7 +26,7 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class Main {
 
-	public static String version='1.2.0beta2.1'
+	public static String version='1.2.0'
 	
 	private static final Logger log=Logger.getLogger(Main.class.name)
 	private static Main main
