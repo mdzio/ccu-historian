f03d957d74901a02084763104c3b735a3b69a32c
Mathias <info@ccu-historian.de>
2019-02-20 07:07:54 +0100

    included highcharts extension

---
+++ ccu-historian/src-webapp/custom/h2-highchart/language.js
@@ -0,0 +1,117 @@
+var ChhLanguage = {
+    'default': {
+        'interface': {
+            'search': 'Suche',
+            'today': 'Heute',
+            'clear': 'Zurücksetzen',
+            'close': 'Schließen',
+            'favorites' : 'Favoriten',
+            'openLink' : 'Link erzeugen',
+            pageTitle: 'CCU-historian: HighChart',
+        },
+        'historian': {
+            //Datenpunkte
+            'interfaceId': 'Schnittstelle',
+            'address': 'Seriennummer:Kanal',
+            'identifier': 'Parameter',
+            'preprocType': 'preprocType', //?
+            'preprocParam': 'preprocParam', //?
+            'displayName': 'Kanalname',
+            'room': 'Raum',
+            'function': 'Gewerk',
+            'comment': 'Kommentar',
+            'paramSet': 'Parameter Set', // ?
+            'tabOrder': 'Tab Reihenfolge', // ?
+            'maximum': 'Maximum',
+            'unit': 'Einheit',
+            'minimum': 'Minimum',
+            'control': 'Typ', //?
+            'operations': 'Operations', //??
+            'flags': 'Flags', //?
+            'type': 'Datentyp',
+            'defaultValue': 'Standard Wert',
+            'idx': 'Historian Id',
+
+            // Std. Gewerke
+            '${funcHeating}': 'Heizung',
+            '${funcButton}': 'Schalter',
+            '${funcLight}': 'Licht',
+            '${funcEnergy}': 'Energiemanagement',
+            '${funcWeather}': 'Wetter',
+
+            //Std. Räume
+            '${roomBedroom}': 'Schlafzimmer',
+            '${roomOffice}': 'Büro',
+            '${roomKitchen}': 'Küche',
+            '${roomChildrensRoom1}': 'Kinderzimmer 1',
+            '${roomGarden}': 'Garten',
+            '${funcSecurity}': 'Sicherheit',
+
+            // Select Option Filter default
+            roomALL: 'Datenpunkte von allen Räumen',
+            sysvarALL: 'Systemvariablen',
+            functionALL: 'Datenpunkte von allen Gewerken',
+
+            // Button and Screen text
+            buttonDay: 'Tag',
+            buttonWeek: 'Woche',
+            buttonMonth: 'Monat',
+            buttonALL: 'Alles',
+            buttonRefresh: 'Refresh',
+            buttonLink: 'Link erzeugen',
+            labelValues: 'Werte',
+            filterPlaceHolder: 'Suchen ...',
+
+        },
+        'highcharts': {
+            decimalPoint: ',',
+            thousandsSep: '.',
+            loading: 'Daten werden geladen...',
+            months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+            weekdays: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+            shortMonths: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+            exportButtonTitle: 'Exportieren',
+            printButtonTitle: 'Drucken',
+            rangeSelectorFrom: 'Von',
+            rangeSelectorTo: 'Bis',
+            rangeSelectorZoom: '', //'Zeitraum',
+            axisYpercentage: 'Prozent %',
+            axisYstate: 'Zustand',
+            downloadPNG: 'Download PNG-Bild',
+            downloadJPEG: 'Download JPEG-Bild',
+            downloadPDF: 'Download PDF-Dokument',
+            downloadSVG: 'Download SVG-Bild',
+            legendactive: 'Legende aktivieren',
+            legenddeactive: 'Legende deaktivieren',
+            navigatoractive: 'Navigator aktivieren',
+            navigatordeactive: 'Navigator deaktivieren',
+            labelsactive: 'Beschriftung aktivieren',
+            labelsdeactive: 'Beschriftung deaktivieren',
+            daylightactive: 'Tag/Nacht aktivieren',
+            daylightdeactive: 'Tag/Nacht deaktivieren',
+            limitactive: 'alle DP anzeigen',
+            limitdeactive: 'nur aktive DP',
+            aggractive1: 'dyn. Aggregation',
+            aggractive2: 'dyn. Aggr. Delta',
+            aggrdeactive: 'keine Aggregation',
+            aggrtxt0: 'keine Aggregation', 
+            aggrtxt1: 'Aggregation', 
+            resetZoom: 'Zoom zurücksetzen',
+            resetZoomTitle: 'Zoom zurücksetzen',
+            range30M: '30M',
+            rangeH: 'H',
+            range6H: '6H',
+            rangeD: 'D',
+            rangeW: 'W',
+            rangeM: 'M',
+            rangeY: 'Y',
+            rangeALL: 'All',
+            aggrmonth: 'Monat',
+            aggrweek: 'Woche',
+            aggrday: 'Tag',
+            aggrhour: 'Stunde',
+            aggrminute: 'Minute',
+            aggrsecond: 'Sekunde',
+        }
+    }
+};
