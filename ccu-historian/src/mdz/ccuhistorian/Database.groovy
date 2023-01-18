e57b50e6a407738851db09af1f7eba8549cd030e
mdzio <info@ccu-historian.de>
2023-01-18 21:35:28 +0100

    placeholder for version in backup file name, closes #375

--- ccu-historian/src/mdz/ccuhistorian/Database.groovy
+++ ccu-historian/src/mdz/ccuhistorian/Database.groovy
@@ -723,13 +723,14 @@ public class Database implements Storage {
 	public static String formatTimestamp(String pattern, Date timestamp) {
 		Calendar cal=Calendar.instance
 		cal.time=timestamp
-		pattern.replaceAll(~/%([%YMWDh])/, { List<String> captures ->
+		pattern.replaceAll(~/%([%YMWDhV])/, { List<String> captures ->
 			switch (captures[1]) {
 				case 'Y': cal[Calendar.YEAR]; break
 				case 'M': ((cal[Calendar.MONTH]-Calendar.JANUARY+1) as String).padLeft(2, '0'); break
 				case 'W': (cal[Calendar.WEEK_OF_YEAR] as String).padLeft(2, '0'); break
 				case 'D': (cal[Calendar.DAY_OF_MONTH] as String).padLeft(2, '0'); break
 				case 'h': (cal[Calendar.HOUR_OF_DAY] as String).padLeft(2, '0'); break
+				case 'V': Main.version; break
 				case '%': '%'; break
 			}
 		})
