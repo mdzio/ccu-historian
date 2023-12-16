f28f7f0d63f0fb32356a913b184af7c366acc3bc
mdzio <info@ccu-historian.de>
2023-12-16 14:43:08 +0100

    sorting of data point lists, closes #299, closes #98 add-on: set language to DE, closes #400 always generate version info in build script

--- ccu-historian/src/mdz/ccuhistorian/Main.groovy
+++ ccu-historian/src/mdz/ccuhistorian/Main.groovy
@@ -24,7 +24,7 @@ import mdz.hc.persistence.Storage
 
 class Main {
 
-	public static String version='3.5.0-beta.2'
+	public static String version='3.5.0-beta.1'
 	
 	private static final Logger log=Logger.getLogger(Main.class.name)
 	private static Main main
