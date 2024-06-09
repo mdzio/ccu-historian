2b4da1ebc1c4208e1c1e5de4d270670e89fbbe7f
mdzio <info@ccu-historian.de>
2024-06-09 12:21:39 +0200

    bump version to 4.0.1

--- ccu-historian/src/mdz/ccuhistorian/Main.groovy
+++ ccu-historian/src/mdz/ccuhistorian/Main.groovy
@@ -24,7 +24,7 @@ import mdz.hc.persistence.Storage
 
 class Main {
 
-	public static String version='4.0.0'
+	public static String version='4.0.1'
 	
 	private static final Logger log=Logger.getLogger(Main.class.name)
 	private static Main main
