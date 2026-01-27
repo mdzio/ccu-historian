3c0bb2168048ce74bd06b7e838b06b4a956e4690
mdzio <info@ccu-historian.de>
2026-01-27 22:47:46 +0100

    update groovy vesion to 4.0.29, fixes #450

--- ccu-historian/src/mdz/ccuhistorian/Main.groovy
+++ ccu-historian/src/mdz/ccuhistorian/Main.groovy
@@ -24,7 +24,7 @@ import mdz.hc.persistence.Storage
 
 class Main {
 
-	public static String version='4.1.2'
+	public static String version='4.1.3'
 	
 	private static final Logger log=Logger.getLogger(Main.class.name)
 	private static Main main
